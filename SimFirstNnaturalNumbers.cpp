0714f46e3811b113661c25164044b164df9519d5
Bunyod Yokubov <bunyodyokubov3102@gmail.com>
2024-07-11 16:12:41 +0500

    more

--- SimFirstNnaturalNumbers.cpp
+++ SimFirstNnaturalNumbers.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int n, i, sum = 0;
-	cout << "Enter n" << endl;
-	cin >> n;
-
-	for (i= 1; i++;)
-	{
-		cout << "Sum of n natural numbers is " << sum;
-	}
-}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n, i, sum = 0;
+//	cout << "Enter n" << endl;
+//	cin >> n;
+//
+//	for (i= 1; i++;)
+//	{
+//		cout << "Sum of n natural numbers is " << sum;
+//	}
+//}
